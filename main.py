c5ec9a9cb1aaa1fe476fc4cedeaffc7c03f6fb8d
bamboo537 <1413242518@qq.com>
2025-10-19 12:28:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 input_str="abc123你好!@#"
-letter_count = 0
-digit_count = 0
-space_count = 0
-other_count = 0
+letter_count=0
+digit_count=0
+space_count=0
+other_count=0
 for char in input_str:
     if char.isalpha():
-        letter_count += 1
+        letter_count+= 1
     elif char.isdigit():
-        digit_count += 1
+        digit_count+= 1
     elif char.isspace():
-        space_count += 1
+        space_count+= 1
     else:
         other_count += 1
-print(f"英文字符: {letter_count}")
-print(f"数字: {digit_count}")
-print(f"空格: {space_count}")
-print(f"其他字符: {other_count}")
+print(f"英文字符:{letter_count}")
+print(f"数字:{digit_count}")
+print(f"空格:{space_count}")
+print(f"其他字符:{other_count}")
